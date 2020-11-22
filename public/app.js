36defa4e25f9e2a87a8d6b0e2a9e816757b2941e
dmacdermott <damianmacdermott@gmail.com>
2020-11-22 23:58:13 +0900

    started adding edit option to front end

--- public/app.js
+++ public/app.js
@@ -74,40 +74,46 @@ async function getStartingData() {
   document.getElementById("to-do-stat").innerHTML = `${todoTotal}`;
 
   loadProjects(projects);
+  document
+    .querySelectorAll(".edit-link")
+    .forEach(element =>
+      element.addEventListener("click", () =>
+        edit(element.getAttribute("value"))
+      )
+    );
 }
 const projectArea = document.getElementById("project-area");
-
-const loadProjects = function (data) {
-  data.forEach(data => {
+const loadProjects = async function (data) {
+  await data.forEach(data => {
     let projectCard = document.createElement("section");
     projectCard.classList.add("project-card");
     projectArea.appendChild(projectCard);
     projectCard.innerHTML = `
-    <div class="edit-wrapper"><a href="#"> <svg class="edit-icon" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <div class="edit-wrapper"><a href="#" value="${data.projectName}" class="edit-link"> <svg class="edit-icon" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
   </svg></a></div>
       <div class="project-header">
-        <h1><a href="${data.link}" target="_blank">${data.projectName}</a></h1>
-        <h5>${data.type}</h5>
+        <h1><a href="${data.link}" target="_blank" contenteditable="false">${data.projectName}</a></h1>
+        <h5 contenteditable="false">${data.type}</h5>
         
       </div>
       <div class="progress-wrapper">
         <div class="req">
           <h4>BASIC</h4>
-          <div class="status" id="basic-status">${data.basic_req}</div>
+          <div class="status" id="basic-status"contenteditable="false">${data.basic_req}</div>
         </div>
         <div class="req">
           <h4>MEDIUM</h4>
-          <div class="status" id="med-status">${data.med_req}</div>
+          <div class="status" id="med-status"contenteditable="false">${data.med_req}</div>
         </div>
         <div class="req">
           <h4>ADVANCED</h4>
-          <div class="status" id="adv-status">${data.adv_req}</div>
+          <div class="status" id="adv-status"contenteditable="false">${data.adv_req}</div>
         </div>
         <div class="req">
           <h4>NIGHTMARE</h4>
-          <div class="status" id="nightmare-status">${data.nightmare}</div>
+          <div class="status" id="nightmare-status"contenteditable="false">${data.nightmare}</div>
         </div>
       </div>
       `;
@@ -150,4 +156,29 @@ reqSelector.addEventListener("change", async event => {
   projectArea.innerHTML = "";
   loadProjects(reqProjects);
 });
+
+// //Make editable
+async function edit(projectName) {
+  event.preventDefault();
+  //get project data
+  const data = await getAProject(projectName);
+  console.log(data);
+  let editLink = document.getElementById("edit-link");
+  editLink.value = data.link;
+  let editName = document.getElementById("edit-name");
+  editName.value = data.projectName;
+  let editType = document.getElementById("edit-type");
+  editType.value = data.type;
+  let editBasicReq = document.getElementById("edit-basic");
+  editBasicReq.value = data.basic_req;
+  let editMediumReq = document.getElementById("edit-medium");
+  editMediumReq.value = `${data.med_req}`;
+  let editAdvReq = document.getElementById("edit-adv");
+  editAdvReq = `${data.adv_req}`;
+  let editNightmare = document.getElementById("edit-nightmare");
+  editNightmare = `${data.nightmare}`;
+  let editToDo = document.getElementById("edit-todo");
+  editToDo.value = `${data.todo}`;
+}
+
 getStartingData();
