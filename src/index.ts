eea9f994b8bcdd58274d9f923b9cded7ba890a53
dmacdermott <damianmacdermott@gmail.com>
2020-11-22 09:43:42 +0900

    load projects and project done

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,48 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+// import {User} from "./entity/User";
 import * as express from "express";
+import Projects from "./entity/Projects";
 
 
-
+//Create connection to db
 createConnection().then(async connection => {
+    const projectsRepository = connection.getRepository(Projects);
+
+    //Create and setup server
     const app = express()
+    app.use(express.json());
+
+    
+    //Rest Routes
+    //GET
+    //Get all projects
+     app.get("/projects", async function(req, res) {
+         console.log(req, res)
+        const projects = await projectsRepository.find();
+     res.json(projects)
+    });
+    //Get one project
+    app.get("/projects/:id", async function(req, res) {
+        console.log(req.params)
+        const results = await projectsRepository.findOne(req.params.id);
+        return res.send(results);
+    });
+    app.post("/projects", async function(req, res) {
+        console.log(req.body)
+        const project = await projectsRepository.create(req.body)
+        const results = await projectsRepository.save(project);
+        return res.send(results)
+    });
+    app.get("/projects", async function(req, res) {
+        const projects = await projectsRepository.find();
+     res.json(projects)
+    });
+
+    //Start server
     const port = 3000
     app.listen(port, ()=>{
         console.log(`Server started on port ${port}`)
     })
-    
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
     
 }).catch(error => console.log(error));
